a065a84884bf9b49ee802b1e41f54f3e59fc05d7
kai.1001 <kai.1001@kakaocorp.com>
2018-10-29 19:46:18 +0900

    custom token store

--- src/main/java/com/tistory/lky1001/oauth2/config/OAuth2AuthorizationServerConfig.java
+++ src/main/java/com/tistory/lky1001/oauth2/config/OAuth2AuthorizationServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.token.DefaultToken;
 import org.springframework.security.core.token.Token;
 import org.springframework.security.core.token.TokenService;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -21,7 +22,9 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.*;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 import java.util.*;
 
@@ -152,116 +155,15 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
     @Bean
     public TokenStore tokenStore() {
-        return new TokenStore() {
-            @Override
-            public OAuth2Authentication readAuthentication(OAuth2AccessToken token) {
-                logger.debug("readAuthentication");
-                return null;
-            }
-
-            @Override
-            public OAuth2Authentication readAuthentication(String token) {
-                logger.debug("readAuthentication");
-                return null;
-            }
-
-            @Override
-            public void storeAccessToken(OAuth2AccessToken token,
-                    OAuth2Authentication authentication) {
-                logger.debug("storeAccessToken");
-            }
-
-            @Override
-            public OAuth2AccessToken readAccessToken(String tokenValue) {
-                logger.debug("readAccessToken");
-                return null;
-            }
-
-            @Override
-            public void removeAccessToken(OAuth2AccessToken token) {
-                logger.debug("removeAccessToken");
-            }
-
-            @Override
-            public void storeRefreshToken(OAuth2RefreshToken refreshToken,
-                    OAuth2Authentication authentication) {
-                logger.debug("storeRefreshToken");
-            }
-
-            @Override
-            public OAuth2RefreshToken readRefreshToken(String tokenValue) {
-                logger.debug("readRefreshToken");
-                return null;
-            }
-
-            @Override
-            public OAuth2Authentication readAuthenticationForRefreshToken(
-                    OAuth2RefreshToken token) {
-                logger.debug("readAuthenticationForRefreshToken");
-                return null;
-            }
-
-            @Override
-            public void removeRefreshToken(OAuth2RefreshToken token) {
-                logger.debug("removeRefreshToken");
-            }
-
-            @Override
-            public void removeAccessTokenUsingRefreshToken(OAuth2RefreshToken refreshToken) {
-                logger.debug("removeAccessTokenUsingRefreshToken");
-            }
-
-            @Override
-            public OAuth2AccessToken getAccessToken(OAuth2Authentication authentication) {
-                logger.debug("getAccessToken");
-                return null;
-            }
-
-            @Override
-            public Collection<OAuth2AccessToken> findTokensByClientIdAndUserName(String clientId,
-                    String userName) {
-                logger.debug("findTokensByClientIdAndUserName");
-                return null;
-            }
-
-            @Override
-            public Collection<OAuth2AccessToken> findTokensByClientId(String clientId) {
-                logger.debug("findTokensByClientId");
-                return null;
-            }
-        };
+        return new InMemoryTokenStore();
     }
 
     @Autowired
     private CustomUserDetailService userDetailService;
 
-    @Bean
-    public AuthorizationServerTokenServices tokenService() {
-        return new AuthorizationServerTokenServices() {
-
-            @Override
-            public OAuth2AccessToken createAccessToken(OAuth2Authentication authentication)
-                throws AuthenticationException {
-                return null;
-            }
-
-            @Override
-            public OAuth2AccessToken refreshAccessToken(String refreshToken,
-                TokenRequest tokenRequest) throws AuthenticationException {
-                return null;
-            }
-
-            @Override
-            public OAuth2AccessToken getAccessToken(OAuth2Authentication authentication) {
-                return null;
-            }
-        };
-    }
-
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore)
-            .tokenServices(tokenService())
             .authenticationManager(authenticationManager)
             .userDetailsService(userDetailService);
     }
--- src/main/java/com/tistory/lky1001/oauth2/config/OAuth2ResourceServerConfig.java
+++ src/main/java/com/tistory/lky1001/oauth2/config/OAuth2ResourceServerConfig.java
@@ -1,12 +1,29 @@
 package com.tistory.lky1001.oauth2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 
 @Configuration
 @EnableResourceServer
@@ -16,8 +33,225 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(SPARKLR_RESOURCE_ID);
+        resources.resourceId(SPARKLR_RESOURCE_ID)
+                .tokenStore(tokenStore());
     }
+
+    public static class CustomOAuth2Request extends OAuth2Request {
+        public CustomOAuth2Request(String clientId) {
+            super(clientId);
+        }
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new TokenStore() {
+            @Override
+            public OAuth2Authentication readAuthentication(OAuth2AccessToken token) {
+                OAuth2Request storedRequest = new CustomOAuth2Request(SPARKLR_RESOURCE_ID);
+                storedRequest.getResourceIds().add(SPARKLR_RESOURCE_ID);
+
+                OAuth2Authentication bizUserOauth2Authentication = new OAuth2Authentication(storedRequest, new Authentication() {
+
+                    @Override
+                    public String getName() {
+                        return null;
+                    }
+
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getCredentials() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getDetails() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getPrincipal() {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isAuthenticated() {
+                        return false;
+                    }
+
+                    @Override
+                    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+                    }
+                });
+
+                return bizUserOauth2Authentication;
+            }
+
+            @Override
+            public OAuth2Authentication readAuthentication(String token) {
+                return null;
+            }
+
+            @Override
+            public void storeAccessToken(OAuth2AccessToken token, OAuth2Authentication authentication) {
+
+            }
+
+            @Override
+            public OAuth2AccessToken readAccessToken(String tokenValue) {
+                return new OAuth2AccessToken() {
+                    @Override
+                    public Map<String, Object> getAdditionalInformation() {
+                        return null;
+                    }
+
+                    @Override
+                    public Set<String> getScope() {
+                        return null;
+                    }
+
+                    @Override
+                    public OAuth2RefreshToken getRefreshToken() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getTokenType() {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isExpired() {
+                        return false;
+                    }
+
+                    @Override
+                    public Date getExpiration() {
+                        return null;
+                    }
+
+                    @Override
+                    public int getExpiresIn() {
+                        return 0;
+                    }
+
+                    @Override
+                    public String getValue() {
+                        return tokenValue;
+                    }
+                };
+            }
+
+            @Override
+            public void removeAccessToken(OAuth2AccessToken token) {
+
+            }
+
+            @Override
+            public void storeRefreshToken(OAuth2RefreshToken refreshToken, OAuth2Authentication authentication) {
+
+            }
+
+            @Override
+            public OAuth2RefreshToken readRefreshToken(String tokenValue) {
+                return null;
+            }
+
+            @Override
+            public OAuth2Authentication readAuthenticationForRefreshToken(OAuth2RefreshToken token) {
+                return null;
+            }
+
+            @Override
+            public void removeRefreshToken(OAuth2RefreshToken token) {
+
+            }
+
+            @Override
+            public void removeAccessTokenUsingRefreshToken(OAuth2RefreshToken refreshToken) {
+
+            }
+
+            @Override
+            public OAuth2AccessToken getAccessToken(OAuth2Authentication authentication) {
+                return null;
+            }
+
+            @Override
+            public Collection<OAuth2AccessToken> findTokensByClientIdAndUserName(String clientId, String userName) {
+                return null;
+            }
+
+            @Override
+            public Collection<OAuth2AccessToken> findTokensByClientId(String clientId) {
+                return null;
+            }
+        };
+    }
+
+//    @Bean
+//    public ResourceServerTokenServices resourceServerTokenServices() {
+//        return new ResourceServerTokenServices() {
+//            @Override
+//            public OAuth2Authentication loadAuthentication(String accessToken) throws AuthenticationException, InvalidTokenException {
+//                OAuth2Request storedRequest = new CustomOAuth2Request(SPARKLR_RESOURCE_ID);
+//
+//                OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(storedRequest, new Authentication() {
+//                    @Override
+//                    public Collection<? extends GrantedAuthority> getAuthorities() {
+//                        return Arrays.asList(new GrantedAuthority() {
+//                            @Override
+//                            public String getAuthority() {
+//                                return "ROLE_USER";
+//                            }
+//                        });
+//                    }
+//
+//                    @Override
+//                    public Object getCredentials() {
+//                        return "melisa";
+//                    }
+//
+//                    @Override
+//                    public Object getDetails() {
+//                        return null;
+//                    }
+//
+//                    @Override
+//                    public Object getPrincipal() {
+//                        return "koala";
+//                    }
+//
+//                    @Override
+//                    public boolean isAuthenticated() {
+//                        return false;
+//                    }
+//
+//                    @Override
+//                    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+//
+//                    }
+//
+//                    @Override
+//                    public String getName() {
+//                        return "melisa";
+//                    }
+//                });
+//
+//                return oAuth2Authentication;
+//            }
+//
+//            @Override
+//            public OAuth2AccessToken readAccessToken(String accessToken) {
+//                return null;
+//            }
+//        };
+//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
